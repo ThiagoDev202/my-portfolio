c8b70b5c6c39f08823a271e200dcf8fc760ca9dc
ThiagoDev202 <thiago2002.fernandes@hotmail.com>
2024-07-24 23:28:43 -0300

    fix: navbar

---
+++ src/components/About.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <section id="about">
+      <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
+        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+          <h1 className="title-font sm:text-4xl text-3xl font-bold mb-4 font-medium text-gray-800">
+            Olá, bem-vindo ao meu portfolio!
+            {/* <br className="hidden lg:inline-block" />Full-Stack Developer */}
+          </h1>
+          <p className="mb-8 leading-relaxed my-2 text-gray-800">
+            Desenvolvedor Full-Stack com mais de 1 ano de experiência na área.
+          </p>
+          <div className="flex justify-center">
+            <a
+              href="#contact"
+              className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
+              Contato
+            </a>
+            <a
+              href="#projects"
+              className="ml-4 inline-flex text-white bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
+              Veja meus projetos
+            </a>
+          </div>
+        </div>
+        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+          <img
+            className="object-cover object-center rounded"
+            alt="hero"
+            src="./coding.svg"
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
